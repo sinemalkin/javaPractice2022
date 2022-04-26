6b35b5c8b4f61283425ef58e8868d44cf0a19144
sinemalkin <sinemalkinn@gmail.com>
2022-04-26 11:51:38 +0300

    Initial commit

---
+++ src/practice_free/Tekrar1.java
@@ -0,0 +1,28 @@
+package practice_free;
+
+public class Tekrar1 {
+    public static void main(String[] args) {
+       /* Bir aydaki gün sayısını hesaplayan bir Java programı yazınız.
+
+                INPUT:
+
+        Ay Numarasi:
+
+        2
+
+        Yil :
+
+        2016
+
+        OUTPUT :
+
+        Subat 2016 29 Gundur.*/
+
+
+
+
+
+
+
+    }
+}
